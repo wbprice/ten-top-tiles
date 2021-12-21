7ab2bd366b64b97d23f6afa57fea1540df1d57d0
Blaine Price <blaine@ario.com>
2021-12-21 16:19:30 -0500

    window size

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use plugins::GamefieldPlugin;
 
 fn main() {
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Ten Top Tiles".to_string(),
+            width: 800.,
+            height: 600.,
+            vsync: true,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(GamefieldPlugin)
         .run();
